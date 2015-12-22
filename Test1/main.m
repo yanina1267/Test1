b93d90456e45f197a9d212ffc53c1be098ecdb8d
Pavlo Muratov <pavlo.muratov@thinkmobiles.com>
2015-12-22 22:49:08 +0200

    Initial Commit

---
+++ Test1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Test1
+//
+//  Created by Pavlo on 12/22/15.
+//  Copyright © 2015 Thinkmobiles. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}

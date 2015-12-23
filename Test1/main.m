3e036591ab095793326b4d8dab5fb3aac3c3abac
Pavlo Muratov <pavlo.muratov@thinkmobiles.com>
2015-12-23 22:48:10 +0200

    working with NSDate

--- Test1/main.m
+++ Test1/main.m
@@ -8,10 +8,17 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool
+    {
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSDate *now = [[NSDate alloc] init];
+        NSLog(@"THe date is %@", now);
+        double seconds = [now timeIntervalSince1970];
+        NSLog(@"It has been %f seconds since the start of 1970.", seconds);
+        NSDate *later = [now dateByAddingTimeInterval:100000];
+        NSLog(@"In 100,000 seconds will be %@", later);
     }
     return 0;
 }
